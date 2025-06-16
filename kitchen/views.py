bcbb596521444df9b0fedd2c29c64aa5c7a987e3
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-06-16 18:18:57 +0200

    Add model lists urls
    Create new urls.py and create model-list url to each model

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request):
+    """View function for the home page of site."""
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+    }
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "kitchen/dish_type_list.html"
+    paginate_by = 10
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    paginate_by = 10
+
+
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    paginate_by = 10
